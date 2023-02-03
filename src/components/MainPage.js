677fd5ae43fcc610766ad507859ecef94fb59170
junujunuyu <yjw4110@naver.com>
2023-02-03 09:57:56 +0900

    mango_shop

---
+++ src/components/MainPage.js
@@ -0,0 +1,60 @@
+import React, {useEffect} from 'react';
+import './MainPage.css';
+import axios from 'axios';
+
+const MainPage = () => {
+    let[products,setProducts] = React.useState([]);
+    useEffect(()=>{
+        axios.get("https://40750f2d-9f9e-44f9-a7e2-1a94c73286f5.mock.pstmn.io/products")
+        .then((result)=>{
+            const products=result.data.products;
+            setProducts(products);
+        }).catch((error)=>{
+            console.log(`통신실패:${error}`)
+        });
+    },[]);
+
+    return (
+    <div>
+        <div id="header">
+        <div id="header-area">
+            <img src="./images/icons/logo.png" alt="logo"/>
+        </div>
+        </div>
+        <div id="body">
+            <div id="banner">
+                <img src="./images/banners/banner1.png" alt="mainimg"/>
+            </div>
+            <h1>Products</h1>
+            <div id="product-list">
+                {products.map((product,idx) => {
+                    /* console.log(product); */
+                    return (
+                        <div className="product-card" key={idx}>
+                            <div>
+                                <img src={product.imageUrl} alt={product.name} className="product-img" />
+                            </div>
+                            <div className="product-contents">
+                                <span className="product-name">{product.name}</span>
+                                <span className="product-price">{product.price}</span>
+                                <span className="product-seller">
+                                    <img src="./images/icons/avatar.png" alt="avatar" className="product-avatar" />
+                                    <span>{product.seller}</span>
+                                </span>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+        <div id="footer">
+            <a href="#!">회사소개</a>
+            <a href="#!">이용약관</a>
+            <a href="#!">통신판매업:123-1234</a>
+            <a href="#!">사업자등록번호:456-56-789654</a>
+        </div>
+    </div>
+    );
+};
+
+export default MainPage;
