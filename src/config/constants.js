215b618aeee851b2fecc1611f7377a51abca838c
junujunuyu <yjw4110@naver.com>
2023-02-10 13:14:28 +0900

    api_변경함

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,1 @@
-export const API_URL=process.env.NODE_ENV === 'production'?"https://port-0-mango-server-3a9t2bldxxgfc1.sel3.cloudtype.app/":"http://localhost:8080";
+export const API_URL=process.env.NODE_ENV === 'production'?"https://port-0-mango-server-3a9t2bldxxgfc1.sel3.cloudtype.app":"http://localhost:8080";
